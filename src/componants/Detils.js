adc30110446b5c0a13a6e1a91a840c9d59dbe592
Hussam <h.said.c@nhc.sa>
2022-07-28 13:54:26 +0300

    fix: add router by id

--- src/componants/Detils.js
+++ src/componants/Detils.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
 
+const Detils = () => {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
 
-
-
-const Detils =()=> {
-    const data = useParams()
-//const  {item} = data.state
-
-    console.log();
-    debugger
-
-
+  console.log("id: ", id);
 
   return (
-    <div className='detils'>
-<img className='image' src='https://t4.ftcdn.net/jpg/00/97/00/09/360_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg'/>
+    <div className="detils">
+      <img
+        className="image"
+        src="https://t4.ftcdn.net/jpg/00/97/00/09/360_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg"
+      />
 
-<p>Power:</p>
-<p>Description:</p>
-
+      <p>Power:</p>
+      <p>Description:</p>
     </div>
-  )
-}
+  );
+};
 
-export default Detils
+export default Detils;
--- src/componants/Table.jsx
+++ src/componants/Table.jsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react'
-import ReactStars from 'react-rating-stars-component';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import Detils from './Detils';
+import React, { useState } from "react";
+import ReactStars from "react-rating-stars-component";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Detils from "./Detils";
 
+function Table({ data, navigation }) {
+  const [rating, setRating] = useState(null);
+  const navigate = useNavigate();
 
-function Table({data,navigation}) {
-    const [rating,setRating] = useState(null);
-    const navigate = useNavigate();
   return (
     <table className="table">
-
-
-    <tbody>
-                <tr >
-                    <th>Hero Name</th>
-                    <th>Power</th>
-                    <th>Rates</th>
-                </tr>
-                {data.map((item=>
-                <tr key={item.id}>
-
-                    <td><Link to={"/detils"} state={{item}}>{item.hero_name}</Link></td>
-                    <td>{item.power}</td>
-                    <td>{<ReactStars/>}</td>
-                </tr>
-                ))}
-            </tbody>
-            </table>
-  )
+      <tbody>
+        <tr>
+          <th>Hero Name</th>
+          <th>Power</th>
+          <th>Rates</th>
+        </tr>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>
+              <Link to={`/detils?id=${item.id}`} state={{ item }}>
+                {item.hero_name}
+              </Link>
+            </td>
+            <td>{item.power}</td>
+            <td>{<ReactStars />}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-export default Table
+export default Table;
